6c64a996e7ec97f17b68f6bbe5cf1a4a35f80249
Daniel McDermott <66064938+dannymc1705@users.noreply.github.com>
2020-05-29 11:23:56 +0100

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,11 @@
+function fuck( ) {
+  var name = document.getElementById("name").value;
+  if (name == ""){
+  var counter = "Fuck You, You No Name Bitch"
+  }
+  else{
+ counter  = "Fuck You " + name
+  }
+
+ document.getElementById("insult").innerHTML = counter ;
+}
